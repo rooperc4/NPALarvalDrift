b212ec55954407b4c6e2e996b0b58017919b5401
Chris Rooper <chris.rooper@dfo-mpo.gc.ca>
2021-03-19 17:10:58 -0700

    Fixed sorting error and added MODIS function

--- R/Functions.R
+++ R/Functions.R
@@ -178,12 +178,21 @@ AVHRR_daily_extract_function<-function(Longitude,Latitude,date,destination_path,
   require(httr)
   require(sp)
 
-  Latitude<-ceiling(abs(Latitude)*4)/4*Latitude/abs(Latitude)
-  Longitude<-ceiling(abs(Longitude)*4)/4*Longitude/abs(Longitude)
+  #  file1<-nc_open("D:/AVHRR Data/oisst-avhrr-v02r01.20001115.nc")
+  # Longitude<-alltraj$Lon[1:4]
+  # Latitude<-alltraj$Lat[1:4]
+  # date<-alltraj$date[1:4]
+  # do_download<-FALSE
+  # Area_of_interest<-c(-230,-120,15,60)
+  # destination_path<-"D:/AVHRR Data/"
+
+  Latitude_trim<-ceiling(abs(Latitude)*4)/4*Latitude/abs(Latitude)
+  Longitude_trim<-ceiling(abs(Longitude)*4)/4*Longitude/abs(Longitude)
 
 
 
   if(is.vector(Area_of_interest)){
+    #  Area_of_interest<-c(130,-120,15,60)
     x_coords<-c(Area_of_interest[1],Area_of_interest[1],Area_of_interest[2],Area_of_interest[2],Area_of_interest[1])
     y_coords<-c(Area_of_interest[3],Area_of_interest[4],Area_of_interest[4],Area_of_interest[3],Area_of_interest[3])
     Area_of_interest <- sp::Polygon(cbind(x_coords,y_coords))
@@ -192,13 +201,20 @@ AVHRR_daily_extract_function<-function(Longitude,Latitude,date,destination_path,
   }
 
   year<-as.numeric(unique(format(date,"%Y")))
+  #year<-seq(Year_min,Year_max,1)
   destpath<-destination_path
   urlpath<-"https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/v2.1/access/avhrr/"
 
   month<-as.numeric(unique(format(date,"%m")))
+  #month<-seq(1,12,1)
+  #if(Season=="Spring"){month<-seq(3,5,1)}
+  #if(Season=="Winter"){month<-c(12,1,2)}
+  #if(Season=="Summer"){month<-seq(6,8,1)}
+  #if(Season=="Fall"){month<-seq(9,11,1)}
 
   month<-formatC(month,width=2,format="d",flag="0")
   day<-as.numeric(unique(format(date,"%d")))
+  #day<-seq(1,31,1)
   day<-formatC(day,width=2,format="d",flag="0")
 
   if(do_download==TRUE){
@@ -238,10 +254,125 @@ AVHRR_daily_extract_function<-function(Longitude,Latitude,date,destination_path,
           nc_close(file1)}
         AVHRR1<-rbind(AVHRR1,data1)}}}
 
-  AVHRR2<-data.frame(Longitude,Latitude,date)
+  AVHRR2<-data.frame(Longitude,Latitude,Longitude_trim,Latitude_trim,date)
   AVHRR1$Longitude[AVHRR1$Longitude<=(-180)]<-AVHRR1$Longitude[AVHRR1$Longitude<=(-180)]+360
 
-  AVHRR3<-merge(AVHRR2,AVHRR1,by=c("Longitude","Latitude","date"),all.x=TRUE)
-
+  AVHRR3<-merge(AVHRR2,AVHRR1,by.x=c("Longitude_trim","Latitude_trim","date"),by.y=c("Longitude","Latitude","date"),all.x=TRUE)
+  AVHRR3<-AVHRR3[,3:5]
   return(AVHRR3)
+}
+
+
+
+#' Function to extract the Modis data at a date and location
+#'
+#' This function downloads MODIS satellite ocean productivity from the Oregon State University Ocean Productivity website
+#' (http://sites.science.oregonstate.edu/ocean.productivity/standard.product.php) and stores them to a local destination.
+#' It will then extract the chlorophyll data for a set of dates at a set of locations and merge to the original data set.
+#' The satellite data is only available on 8 day intervals, so the output will contain NA where no data were available.
+#' THe Ocean Color is gridded on 1/4 degrees in this output.
+#' @param Longitude Longitude of interest
+#' @param Latitude Latitude of interest
+#' @param date  date of interest
+#' @param destination_path Local path for location of MODIS daily files
+#' @param do_download TRUE (default) or FALSE should the MODIS daily files be downloaded
+#' @param Area_of_interest Either a polygon or x and y min and max defining the area of interest (defauilt is the North Pacific)
+#' @keywords Larval drift, sea surface temperature, AVHRR
+#' @export
+#' @examples
+#' temps<-MODIS_daily_extract_function(alltraj$Lon[1:4],alltraj$Lat[1:4],alltraj$date[1:4],"D:/MODIS data/",do_download=FALSE)
+#'
+#'
+#'
+
+
+MODIS_extract_function<-function(Longitude,Latitude,date,destination_path,do_download=TRUE,Area_of_interest=c(-230,-120,15,60)){
+  require(stringr)
+  require(RCurl)
+  #
+  #    Longitude<-alltraj$Lon[2200000:220500]
+  #    Latitude<-alltraj$Lat[2000000:2000500]
+  #    date<-alltraj$date[2000000:2000500]
+  #    do_download<-FALSE
+  #    Area_of_interest<-c(-230,-120,15,60)
+  #    destination_path<-"D:/MODIS data/"
+  #
+
+
+  ##DOWNLOAD RELEVANT YEARS OF DATA
+  datematrix<-paste(format(date,"%Y"),formatC(format(date,"%j"),width=3,format="d",flag="0"),sep="")
+  datematrix<-unique(datematrix)
+  year<-as.numeric(unique(format(date,"%Y")))
+
+  urlpath<-"http://orca.science.oregonstate.edu/data/1x2/8day/vgpm.r2018.m.chl.m.sst/xyz/"
+  destpath<-destination_path
+
+  if(do_download==TRUE){
+    for(i in 1:length(year)){
+      urlname<-paste(urlpath,"vgpm.m.",year[i],".xyz.tar",sep="")
+      if(url.exists(urlname)==TRUE){
+        if(file.exists(paste(destpath,"vgpm.m.",year[i],".xyz.tar",sep=""))==FALSE){
+          download.file(urlname, destfile=paste(destpath,"vgpm.m.",year[i],".xyz.tar",sep=""),mode="wb", quiet = FALSE)
+          untar(paste(destpath,"vgpm.m.",year[i],".xyz.tar",sep=""),exdir=paste0(destpath,"GZ/"))}}}}
+  #vgpm = net primary production (units of mg C / m**2 / day) based on the standard vgpm algorithm
+  #yyyy = year
+  #ddd = day of year of the start of each 8day file
+  #all = all pixels output, including those with no data (nodata = -9999)
+  #xyz = text file, where x = longitude, y = latitude and z = npp value
+  #gz = compressed with gzip (uncompress with gunzip)
+
+
+  setwd(paste0(destpath,"GZ/"))
+
+  f1<-list.files(full.names=TRUE)
+  f2<-which(as.numeric(str_sub(f1, -18, -12))%in%as.numeric(datematrix))
+  f1<-f1[f2]
+
+
+  if(is.vector(Area_of_interest)){
+    #  Area_of_interest<-c(130,-120,15,60)
+    x_coords<-c(Area_of_interest[1],Area_of_interest[1],Area_of_interest[2],Area_of_interest[2],Area_of_interest[1])
+    y_coords<-c(Area_of_interest[3],Area_of_interest[4],Area_of_interest[4],Area_of_interest[3],Area_of_interest[3])
+    Area_of_interest <- sp::Polygon(cbind(x_coords,y_coords))
+    Area_of_interest <- sp::Polygons(list(Area_of_interest), ID = "Area")
+    Area_of_interest <- sp::SpatialPolygons(list(Area_of_interest),proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs"))
+  }
+
+  data1<-read.table(f1[1],skip=1)
+  spots<-data.frame(Lon=data1[,1],Lat=data1[,2])
+  spots$Lon[spots$Lon>0]<-spots$Lon[spots$Lon>0]-360
+  meuse_sf = SpatialPoints(spots,proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs"))
+  goodpts<-which(sp::over(meuse_sf,Area_of_interest)>0)
+
+  modis1<-array(dim=c(0,4))
+  colnames(modis1)<-c("lon","lat","color","date")
+  for(i in 1:length(f1)){
+
+    data1<-read.table(f1[i],skip=1)
+    data1<-data1[goodpts,]
+    data1<-subset(data1,data1[,3]>-9999)
+    if(length(data1[,1])>0){
+      colnames(data1)<-c("lon","lat","color")
+      data1$date<-as.Date(paste(as.numeric(str_sub(f1[i], -18, -15)),as.numeric(str_sub(f1[i], -14, -12)),sep="-"),"%Y-%j")
+      modis1<-rbind(modis1,data1)}
+  }
+
+
+  modis1$lon[modis1$lon<0]<-modis1$lon[modis1$lon<0]+360
+
+  Latitude_trim<-ceiling(abs(Latitude)*4)/4*Latitude/abs(Latitude)
+  Longitude_trim<-ceiling(abs(Longitude)*4)/4*Longitude/abs(Longitude)
+
+  modis1$lat<-ceiling(abs(modis1$lat)*4)/4*modis1$lat/abs(modis1$lat)
+  modis1$lon<-ceiling(abs(modis1$lon)*4)/4*modis1$lon/abs(modis1$lon)
+  modis1<-aggregate(color~lat+lon+date,data=modis1,FUN="mean")
+
+
+  modis2<-data.frame(Longitude,Latitude,Longitude_trim,Latitude_trim,date)
+
+
+  modis1<-merge(modis2,modis1,by.x=c("Longitude_trim","Latitude_trim","date"),by.y=c("lon","lat","date"),all.x=TRUE)
+  modis1<-modis1[,3:5]
+
+  return(modis1)
 }
